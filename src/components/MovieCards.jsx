fcad1ab78c83db79f419f848b8ba0f384baf02a7
Jacob Ko <tko003@ucr.edu>
2023-02-22 14:13:08 -0800

    Added rounding helper function.
    Also switched default browser to FireFox which is way better for css... what was I doing before

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -13,12 +13,16 @@ export default function MovieCards(props) {
                 // vote_count={movie.vote_count}
                 // release_date={movie.release_date}
 
+    function roundTwoDecimals(num) {
+        return Math.round((num + Number.EPSILON)*100)/100
+    }
+
     const movieMap = props.movies.map(movie => {
         return (
             <div className="movieCard" key={nanoid()}>
                 <h1>{movie.title}</h1>
-                <h3>Popularity: {movie.popularity}</h3>
-                <h4>Rating: {Math.round((movie.vote_average + Number.EPSILON)*100)/100}</h4>
+                <h3>Popularity: {roundTwoDecimals(movie.popularity)}</h3>
+                <h4>Rating: {roundTwoDecimals(movie.vote_average)}</h4>
             </div>
         )
     })
