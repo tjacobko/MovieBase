27a638d31e74db81ff413b48b39c5bcecac508b2
Jacob Ko <tko003@ucr.edu>
2023-02-22 21:46:06 -0800

    Added poster images to MovieCards.
    Had to use configuration api call.

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,33 @@ import MovieCards from './components/MovieCards'
 
 function App() {
   const [movieData, setMovieData] = useState()
+  const [config, setConfig] = useState()
   
   useEffect(() => {
     const key = import.meta.env.VITE_API_KEY
-
     fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${key}`)
       .then(res => res.json())
       .then(data => setMovieData(data.results))
   }, [])
+
+  useEffect(() => {
+    const key = import.meta.env.VITE_API_KEY
+    fetch(`https://api.themoviedb.org/3/configuration?api_key=${key}`)
+      .then(res => res.json())
+      .then(data => setConfig(data.images))
+  }, [])
   
   movieData && console.log(movieData)
+  config && console.log(config)
 
   return (
     <div>
       <Header />
-      {movieData && <MovieCards movies={movieData} />}
+      {
+        movieData
+        && config
+        && <MovieCards movies={movieData} config={config}/>
+      }
     </div>
   )
 }
--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -3,23 +3,16 @@ import { nanoid } from 'nanoid'
 import '../index.css'
 
 export default function MovieCards(props) {
-
-                // key={nanoid()}
-                // id={movie.id}
-                // title={movie.title}
-                // overview={movie.overview}
-                // popularity={movie.popularity}
-                // vote_average={movie.vote_average}
-                // vote_count={movie.vote_count}
-                // release_date={movie.release_date}
-
     function roundTwoDecimals(num) {
         return Math.round((num + Number.EPSILON)*100)/100
     }
 
+    const base_url = props.config.base_url
+    const size = props.config.poster_sizes[3]
     const movieMap = props.movies.map(movie => {
         return (
             <div className="movieCard" key={nanoid()}>
+                <img src={`${base_url + size + movie.poster_path}`} />
                 <h1>{movie.title}</h1>
                 <h3>Popularity: {roundTwoDecimals(movie.popularity)}</h3>
                 <h4>Rating: {roundTwoDecimals(movie.vote_average)}</h4>
