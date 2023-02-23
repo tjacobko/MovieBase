ed8ed6df21571c69fb4cd6cc655a62c49a2df4b2
Jacob Ko <tko003@ucr.edu>
2023-02-22 23:04:52 -0800

    Added block div with title and list for movies.

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -8,11 +8,11 @@ export default function MovieCards(props) {
     }
 
     const base_url = props.config.base_url
-    const size = props.config.poster_sizes[3]
+    const size = props.config.backdrop_sizes[0]
     const movieMap = props.movies.map(movie => {
         return (
             <div className="movieCard" key={nanoid()}>
-                <img src={`${base_url + size + movie.poster_path}`} />
+                <img src={`${base_url + size + movie.backdrop_path}`} />
                 <h1>{movie.title}</h1>
                 <h3>Popularity: {roundTwoDecimals(movie.popularity)}</h3>
                 <h4>Rating: {roundTwoDecimals(movie.vote_average)}</h4>
@@ -20,5 +20,12 @@ export default function MovieCards(props) {
         )
     })
     
-    return movieMap
+    return (
+        <div className="block">
+            <h2 className="block-title">Top Movies Today</h2>
+            <div className="block-list">
+                {movieMap}
+            </div>
+        </div>
+    )
 }
