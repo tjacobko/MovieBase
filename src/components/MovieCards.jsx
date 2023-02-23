8ee654e70c7f91b3ebb30c3fb70364a9996a9581
Jacob Ko <tko003@ucr.edu>
2023-02-23 14:53:41 -0800

    Added circular-progressbar to display rating.

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import { nanoid } from 'nanoid'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+
 import '../index.css'
+import 'react-circular-progressbar/dist/styles.css'
 
 export default function MovieCards(props) {
     function roundTwoDecimals(num) {
         return Math.round((num + Number.EPSILON)*100)/100
+    }
+
+    function percentage(num) {
+        return Math.round(Math.round((num + Number.EPSILON)*100)/10)
     }
 
     const base_url = props.config.base_url
@@ -12,10 +19,17 @@ export default function MovieCards(props) {
     const movieMap = props.movies.map(movie => {
         return (
             <div className="movieCard" key={nanoid()}>
-                <img src={`${base_url + size + movie.backdrop_path}`} />
-                <h1>{movie.title}</h1>
-                <h3>Popularity: {roundTwoDecimals(movie.popularity)}</h3>
-                <h4>Rating: {roundTwoDecimals(movie.vote_average)}</h4>
+                <img className="movieCard-img" src={`${base_url + size + movie.backdrop_path}`} />
+                <div className="movieCard-rating">
+                    <CircularProgressbar
+                        strokeWidth={12}
+                        styles={buildStyles({textSize: "22px"})}
+                        value={percentage(movie.vote_average)}
+                        text={`${percentage(movie.vote_average)}%`}
+                    />
+                </div>
+                <h1 className="movieCard-title">{movie.title}</h1>
+                <h3 className="movieCard-popularity">Popularity: {roundTwoDecimals(movie.popularity)}</h3>
             </div>
         )
     })
